6b46c2dec0b99d5e9995e560e39f891ac6dd9621
blindlobstar <boryapictures@gmail.com>
2023-10-01 23:43:55 +0200

    twitch auth endpoint

---
+++ internal/backend/database/streamer.go
@@ -0,0 +1,45 @@
+package database
+
+type Streamer struct {
+	ID         int
+	TwitchId   string `db:"twitch_id"`
+	TwitchName string `db:"twitch_name"`
+	SecretCode string `db:"secret_code"`
+}
+
+func (r Repo) CreateStreamer(s *Streamer) error {
+	_, err := r.DB.Exec("INSERT INTO streamers (twitch_name, secret_code) VALUES ($1, $2)", s.TwitchName, s.SecretCode)
+	return err
+}
+
+func (r Repo) GetStreamers(s Streamer) ([]Streamer, error) {
+	res := []Streamer{}
+	req := "SELECT * FROM streamer"
+	if s.TwitchId == "" && s.TwitchName == "" {
+		err := r.DB.Select(res, req)
+		return res, err
+	}
+
+	req += " WHERE"
+	args := []any{}
+
+	if s.TwitchId != "" {
+		req += " twitch_id = ?"
+		args = append(args, s.TwitchId)
+	}
+
+	if s.TwitchName != "" {
+		req += " twitch_name = ?"
+		args = append(args, s.TwitchName)
+	}
+
+	err := r.DB.Select(res, req, args...)
+	return res, err
+}
+
+func (r Repo) GetStreamerById(id int) (*Streamer, error) {
+	res := &Streamer{}
+	err := r.DB.Get(res, "SELECT * FROM streamer WHERE id = $1", id)
+
+	return res, err
+}
---
+++ internal/backend/endpoints/twitch_auth/twitch_auth.go
@@ -0,0 +1,63 @@
+package twitch_auth
+
+import (
+	"donation-alarm/internal/backend/database"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/nicklaw5/helix"
+)
+
+type Twitch struct {
+	Client *helix.Client
+	DB     database.Repo
+}
+
+type AuthRequest struct {
+	Code  string
+	State string
+}
+
+func (t *Twitch) Authenticate(w http.ResponseWriter, r *http.Request) error {
+	var request AuthRequest
+	json.NewDecoder(r.Body).Decode(&request)
+
+	atr, err := t.Client.RequestUserAccessToken(request.Code)
+	if err != nil {
+		return err
+	}
+	if atr.StatusCode != http.StatusOK {
+		log.Printf("error requesting twitch access token. statusCode: %d \nerror: %s \nerrorMessage: %s", atr.StatusCode, atr.Error, atr.ErrorMessage)
+		w.WriteHeader(http.StatusUnauthorized)
+		return nil
+	}
+
+	_, vr, _ := t.Client.ValidateToken(atr.Data.AccessToken)
+	if vr.StatusCode != http.StatusOK {
+		log.Printf("error validating twitch access token. statusCode: %d \nerror: %s \nerrorMessage: %s", atr.StatusCode, atr.Error, atr.ErrorMessage)
+		w.WriteHeader(http.StatusUnauthorized)
+		return nil
+	}
+	existingStreamers, err := t.DB.GetStreamers(database.Streamer{TwitchId: vr.Data.UserID})
+	if err != nil {
+		return err
+	}
+
+	// if user not exists, create one, publish event
+	// and response with internal access and refresh tokens
+	if len(existingStreamers) == 0 {
+		err := t.DB.CreateStreamer(&existingStreamers[0])
+		if err != nil {
+			return err
+		}
+		// TODO: publish event
+
+	} else if len(existingStreamers) > 1 {
+		log.Printf("there is more than one user with same twitchId: %s \n", vr.Data.UserID)
+		return err
+	}
+
+	w.WriteHeader(200)
+	return nil
+}
