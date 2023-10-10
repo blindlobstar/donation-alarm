3b5af69e6e652461f29fc88cc6a00d8170d2efef
blindlobstar <boryapictures@gmail.com>
2023-10-10 21:39:50 +0200

    implemented streamer repo mock and tests for it

---
+++ backend/internal/database/streamer_mock.go
@@ -0,0 +1,55 @@
+package database
+
+type StreamerMock struct {
+	Streamers []Streamer
+}
+
+func (sm *StreamerMock) CreateStreamer(s *Streamer) error {
+	s.ID = len(sm.Streamers)
+	sm.Streamers = append(sm.Streamers, *s)
+
+	return nil
+}
+
+func (sm *StreamerMock) GetStreamers(s Streamer) ([]Streamer, error) {
+
+	if s.TwitchId == "" && s.TwitchName == "" {
+		res := make([]Streamer, len(sm.Streamers))
+		copy(res, sm.Streamers)
+		return res, nil
+	}
+
+	res := make([]Streamer, 0, len(sm.Streamers))
+	if s.TwitchId != "" {
+		for _, streamer := range sm.Streamers {
+			if streamer.TwitchId == s.TwitchId {
+				res = append(res, streamer)
+			}
+		}
+	}
+
+	if s.TwitchName != "" {
+		for _, streamer := range sm.Streamers {
+			if streamer.TwitchName == s.TwitchName {
+				res = append(res, streamer)
+			}
+		}
+	}
+
+	return res, nil
+}
+
+func (sm *StreamerMock) GetStreamerById(id int) (*Streamer, error) {
+	for _, s := range sm.Streamers {
+		if s.ID == id {
+			return &Streamer{
+				ID:         id,
+				TwitchId:   s.TwitchId,
+				TwitchName: s.TwitchName,
+				SecretCode: s.SecretCode,
+			}, nil
+		}
+	}
+
+	return nil, nil
+}
---
+++ backend/internal/database/streamer_mock_test.go
@@ -0,0 +1,105 @@
+package database
+
+import (
+	"testing"
+)
+
+func TestCreateStreamer(t *testing.T) {
+	sm := &StreamerMock{}
+	streamer := &Streamer{
+		TwitchId:   "twitch123",
+		TwitchName: "teststreamer",
+		SecretCode: "secret123",
+	}
+
+	err := sm.CreateStreamer(streamer)
+
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	if streamer.ID != 0 {
+		t.Errorf("Expected ID to be 0, got %d", streamer.ID)
+	}
+
+	if len(sm.Streamers) != 1 {
+		t.Errorf("Expected Streamers slice length to be 1, got %d", len(sm.Streamers))
+	}
+
+	if sm.Streamers[0] != *streamer {
+		t.Errorf("Expected the stored Streamer to be equal to the created Streamer")
+	}
+}
+
+func TestGetStreamers(t *testing.T) {
+	sm := &StreamerMock{}
+	streamer1 := Streamer{
+		TwitchId:   "twitch123",
+		TwitchName: "teststreamer1",
+		SecretCode: "secret123",
+	}
+	streamer2 := Streamer{
+		TwitchId:   "twitch456",
+		TwitchName: "teststreamer2",
+		SecretCode: "secret456",
+	}
+	sm.Streamers = []Streamer{streamer1, streamer2}
+
+	// Test case 1: Get all streamers
+	allStreamers, err := sm.GetStreamers(Streamer{})
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	if len(allStreamers) != 2 {
+		t.Errorf("Expected 2 streamers, got %d", len(allStreamers))
+	}
+
+	// Test case 2: Get streamer by Twitch ID
+	byTwitchID, err := sm.GetStreamers(Streamer{TwitchId: "twitch123"})
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	if len(byTwitchID) != 1 || byTwitchID[0] != streamer1 {
+		t.Errorf("Expected 1 streamer with Twitch ID 'twitch123', got %+v", byTwitchID)
+	}
+
+	// Test case 3: Get streamer by Twitch Name
+	byTwitchName, err := sm.GetStreamers(Streamer{TwitchName: "teststreamer2"})
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	if len(byTwitchName) != 1 || byTwitchName[0] != streamer2 {
+		t.Errorf("Expected 1 streamer with Twitch Name 'teststreamer2', got %+v", byTwitchName)
+	}
+}
+
+func TestGetStreamerById(t *testing.T) {
+	sm := &StreamerMock{}
+	streamer1 := Streamer{
+		ID:         0,
+		TwitchId:   "twitch123",
+		TwitchName: "teststreamer1",
+		SecretCode: "secret123",
+	}
+	sm.Streamers = []Streamer{streamer1}
+
+	// Test case 1: Get a streamer by ID
+	retrievedStreamer, err := sm.GetStreamerById(0)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	if retrievedStreamer == nil {
+		t.Errorf("Expected a valid streamer, got nil")
+	} else if *retrievedStreamer != streamer1 {
+		t.Errorf("Expected the retrieved streamer to be %+v, got %+v", streamer1, *retrievedStreamer)
+	}
+
+	// Test case 2: Get a non-existent streamer by ID
+	retrievedStreamer, err = sm.GetStreamerById(1)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	if retrievedStreamer != nil {
+		t.Errorf("Expected nil for a non-existent streamer, got %+v", *retrievedStreamer)
+	}
+}
